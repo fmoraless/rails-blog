d73f984ba22f7be799cfd67e8170c1f8181002a7
fmoraless <franticzero@gmail.com>
2022-06-13 02:12:56 -0400

    create new template to new articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+  #before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  # GET /articles
+  # GET /articles.json
+  # def index
+  #   @articles = Article.all
+  # end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+end
